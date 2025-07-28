02bc1b65938ea5fc21dd9634ff3b583651990794
jimp <cex@James-MacBook-Air.local>
2025-07-28 20:41:04 +0100

    Initial commit with app setup started

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route, Navigate } from 'react-router'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Login from './Login/Login'
+import Navbar from './Navbar/Navbar'
+function App() {
+
+  return (
+    <main> 
+      <Navbar />
+      <section className='routes'>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Navigate to="/login"/>}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Route>
+      </Routes>
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Login/Login.jsx
@@ -0,0 +1,11 @@
+import "./login.css"
+function Login() {
+
+  return (
+    <section className="loginBox">
+
+    </section>
+  )
+}
+
+export default Login
